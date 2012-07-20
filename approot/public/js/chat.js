8e3faeafdc11e88485ccc6d3de80d7bd5568985d
lassie <lassie116@gmail.com>
2012-07-21 00:07:05 +0900

    refactoring

--- approot/public/js/chat.js
+++ approot/public/js/chat.js
@@ -1,16 +1,14 @@
 (function(){
      function send_handler(ev){
          var mes = $("#mes").val();
-         console.log(mes);
          $("#mes").val("");
          jQuery.post("/post",{"mes":mes});
-         ajax_get();
+         setTimeout(ajax_get,100);
      }
 
      function ajax_get(){
          jQuery.get("/get",{},function(data){
                         var list = eval("("+data+")");
-                        console.log(data);
                         var j_list = $("#list");
                         j_list.find("p").remove();
                         for (var i = 0,l=list.length;i<l;i++){
